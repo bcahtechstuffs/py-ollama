853b525085d48c9182a9059ebf25e6ab87742207
Bcah <143713579+bcahtechstuffs@users.noreply.github.com>
2025-03-01 16:25:28 +0700

    Primary file upload

---
+++ pyollama.py
@@ -0,0 +1,39 @@
+from urllib import response
+import ollama, rich, os
+client = ollama.Client()
+# hi
+def genchat(usr, modelin):
+    rich.print("[italic]Generating reponse...")
+    response = ollama.generate(model=modelin, prompt=usr)
+    if ":" in modelin:
+        modelname = list(modelin.split(":"))
+    else:
+        modelname = list(modelin)
+    rich.print(f"[bold]{modelname[0]}:", response.response)
+    rich.print('''
+[italic]Reponse generated.''')
+
+modellst = ollama.list()
+# start point
+rich.print("[bold blue]py-[bold yellow]ollama")
+print("""[Version: 0.1]
+      
+A Python3-based terminal interface for Ollama.
+Please enter model name below to start:""")
+modelin = str(input("> "))
+if modelin == '/exit':
+    print("Exiting...")
+    exit()
+elif modelin:
+    rich.print("Selected model:", f"{modelin}")
+    rich.print("[blue]Type /exit to exit program.")
+    while True:
+        usr = str(input(">> "))
+        if usr == "/exit":
+            print("Exiting...")
+            exit()
+        else:
+            genchat(usr, modelin)
+else:
+    rich.print("[bold red][!] No model given, exiting...")
+    exit()
